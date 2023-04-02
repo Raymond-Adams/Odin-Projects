9378d25aaaf9814f5bc7e3081ee3edfc1a1479b9
Raymond Adams <102918860+Raymond-Adams@users.noreply.github.com>
2023-04-01 19:19:00 -0700

    Complete the Revisiting assignment

--- rochambeau/javascript.js
+++ rochambeau/javascript.js
@@ -1,58 +1,76 @@
 const getComputerChoice = () => {
-	const choices = ['Rock', 'Paper', 'Scissors'];
-	const randomIndex = Math.floor(Math.random() * 3);
-	return choices[randomIndex];
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
 };
 
 const playRound = (playerSelection, computerSelection) => {
-	const choice1 = playerSelection.toUpperCase();
-	const choice2 = computerSelection.toUpperCase();
+  const choice1 = playerSelection.toUpperCase();
+  const choice2 = computerSelection.toUpperCase();
 
-	const outcomes = {
-		'ROCK': {
-			'ROCK': 'You Tied! Rock ties with rock.',
-			'PAPER': 'You Lost! Paper beats rock.',
-			'SCISSORS': 'You Won! Rock beats scissors.'
-		},
-		'PAPER': {
-			'ROCK': 'You Won! Paper beats rock.',
-			'PAPER': 'You Tied! Paper ties with paper.',
-			'SCISSORS': 'You Lost! Scissors beats paper.'
-		},
-		'SCISSORS': {
-			'ROCK': 'You Lost! Rock beats scissors.',
-			'PAPER': 'You Won! Scissors beats paper.',
-			'SCISSORS': 'You Tied! Scissors ties with scissors.'
-		}
-	};
+  const outcomes = {
+    ROCK: {
+      ROCK: 'You Tied! Rock ties with rock.',
+      PAPER: 'You Lost! Paper beats rock.',
+      SCISSORS: 'You Won! Rock beats scissors.',
+    },
+    PAPER: {
+      ROCK: 'You Won! Paper beats rock.',
+      PAPER: 'You Tied! Paper ties with paper.',
+      SCISSORS: 'You Lost! Scissors beats paper.',
+    },
+    SCISSORS: {
+      ROCK: 'You Lost! Rock beats scissors.',
+      PAPER: 'You Won! Scissors beats paper.',
+      SCISSORS: 'You Tied! Scissors ties with scissors.',
+    },
+  };
 
-	return outcomes[choice1][choice2];
+  return outcomes[choice1][choice2];
 };
 
 // Usage: const Rochambeau = game(); Play a round with game.play();
-const game = (playerChoice) => ({
-	playerScore: 0,
-	computerScore: 0,
-	matches: 0,
+const game = () => ({
+  playerScore: 0,
+  computerScore: 0,
+  matches: 0,
 
-	play () {
-		const computerChoice = getComputerChoice();
-		const playerChoice = prompt('Rock, paper or scissors?');
-		const result = playRound(playerChoice, computerChoice);
+  play(choice) {
+    const computerChoice = getComputerChoice();
+    const playerChoice = choice;
+    const result = playRound(playerChoice, computerChoice);
 
-		// Match the second word in the result string.
-		switch (result.match(/\w+(?=!)/)[0]) {
-		case 'Won':
-			this.playerScore++;
-			break;
-		case 'Tied':
-			break;
-		case 'Lost':
-			this.computerScore++;
-			break;
-		}
-		
-		this.matches++;
-		console.log(result);
-	}
+    // Match the second word in the result string.
+    switch (result.match(/\w+(?=!)/)[0]) {
+      case 'Won':
+        this.playerScore += 1;
+        break;
+      case 'Tied':
+        break;
+      case 'Lost':
+        this.computerScore += 1;
+        break;
+      default:
+    }
+
+    this.matches += 1;
+
+    if (this.playerScore === 5) return `${result} Player has won!`;
+    if (this.computerScore === 5) return `${result} Computer has won!`;
+
+    return `${result} Computer: ${this.computerScore} Player: ${this.playerScore}`;
+  },
+});
+
+const Rochambeau = game();
+const buttons = document.querySelectorAll('button');
+const onClick = (e) => {
+  const button = e.target;
+  const choice = button.className;
+
+  document.querySelector('#results').textContent = Rochambeau.play(choice);
+};
+
+[...buttons].forEach((button) => {
+  button.addEventListener('click', onClick);
 });
